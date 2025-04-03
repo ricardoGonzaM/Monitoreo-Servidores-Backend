c9989c08fab7e09dbed4355ee319b02cb9e521ca
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2025-04-03 10:42:45 -0600

    Second Commit

--- server.js
+++ server.js
@@ -33,24 +33,27 @@ io.on("connection", (socket) => {
       // Obtener estadísticas de red
       const network = await si.networkStats();
 
+      // Validar si networkStats() devuelve datos
+      let networkData = { upload: "N/A", download: "N/A" };
+      if (network && network.length > 0) {
+        networkData = {
+          upload: (network[0].tx_sec / 1024).toFixed(2) + " KB/s",
+          download: (network[0].rx_sec / 1024).toFixed(2) + " KB/s",
+        };
+      }
+
       // Emitir eventos al cliente
       socket.emit("systemStats", {
         cpu: cpu.currentLoad.toFixed(2),
         memory: ((memory.used / memory.total) * 100).toFixed(2),
         disk: ((disk[0].used / disk[0].size) * 100).toFixed(2),
-        network: {
-          upload: (network[0].tx_sec / 1024).toFixed(2) + " KB/s",
-          download: (network[0].rx_sec / 1024).toFixed(2) + " KB/s",
-        },
+        network: networkData,
       });
 
       socket.emit("cpuInfo", { model: cpuInfo.brand });
       socket.emit("memoryInfo", { total: (memory.total / 1073741824).toFixed(2) + " GB" });
       socket.emit("diskInfo", { total: (disk[0].size / 1073741824).toFixed(2) + " GB" });
-      socket.emit("networkInfo", {
-        upload: (network[0].tx_sec / 1024).toFixed(2) + " KB/s",
-        download: (network[0].rx_sec / 1024).toFixed(2) + " KB/s",
-      });
+      socket.emit("networkInfo", networkData);
     } catch (error) {
       console.error("Error obteniendo datos del sistema:", error);
     }
